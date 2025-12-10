4fcf32abef6422149a5e0f860392a60f941782bc
SerhiiKutovyi <wagenrex@gmail.com>
2025-12-10 17:04:51 +0200

    fix note details

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { fetchNoteById } from '@/lib/api';
-import NoteDetailsClient from './NoteDetailsClient ';
+import NoteDetailsClient from './NoteDetails.client';
 
 type Props = {
   params: Promise<{ id: string }>;
