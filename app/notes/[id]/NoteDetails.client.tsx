6c7b813fd9a1fa75a9f11e5074934144d64b3301
SerhiiKutovyi <wagenrex@gmail.com>
2025-12-11 18:35:37 +0200

    fix NoteDetailsClient

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -1,19 +1,28 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'next/navigation';
+import { fetchNoteById } from '@/lib/api';
+
 import css from './NoteDetails.module.css';
-import type { Note } from '@/types/note';
 
-interface NoteDetailsClientProp {
-  note: Note;
-}
+function NoteDetailsClient() {
+  const { id } = useParams<{ id: string }>();
 
-function NoteDetailsClient({ note }: NoteDetailsClientProp) {
+  const { data: note } = useQuery({
+    queryKey: ['note', id],
+    queryFn: () => fetchNoteById(id),
+    refetchOnMount: false,
+  });
+
   return (
     <div className={css.container}>
       <div className={css.item}>
         <div className={css.header}>
-          <h2>{note.title}</h2>
+          <h2>{note?.title}</h2>
         </div>
-        <p className={css.content}>{note.content}</p>
-        <p className={css.date}>{note.createdAt}</p>
+        <p className={css.content}>{note?.content}</p>
+        <p className={css.date}>{note?.createdAt}</p>
       </div>
     </div>
   );
--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,6 +1,12 @@
-import { fetchNoteById } from '@/lib/api';
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from '@tanstack/react-query';
 
 import NoteDetailsClient from './NoteDetails.client';
+
+import { fetchNoteById } from '@/lib/api';
 
 type Props = {
   params: Promise<{ id: string }>;
@@ -8,8 +14,17 @@ type Props = {
 
 async function NoteDetails({ params }: Props) {
   const { id } = await params;
-  const note = await fetchNoteById(id);
+  const queryClient = new QueryClient();
 
-  return <>{note && <NoteDetailsClient note={note} />}</>;
+  await queryClient.prefetchQuery({
+    queryKey: ['note', id],
+    queryFn: () => fetchNoteById(id),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NoteDetailsClient />
+    </HydrationBoundary>
+  );
 }
 export default NoteDetails;
