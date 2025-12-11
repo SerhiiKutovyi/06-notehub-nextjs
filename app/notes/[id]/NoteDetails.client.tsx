c9327154c30f5aa15b772ad5491fd249991fc04a
SerhiiKutovyi <wagenrex@gmail.com>
2025-12-12 01:01:04 +0200

    fix error

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -9,20 +9,40 @@ import css from './NoteDetails.module.css';
 function NoteDetailsClient() {
   const { id } = useParams<{ id: string }>();
 
-  const { data: note } = useQuery({
+  const {
+    data: note,
+    isLoading,
+    error,
+  } = useQuery({
     queryKey: ['note', id],
     queryFn: () => fetchNoteById(id),
     refetchOnMount: false,
   });
 
+  if (isLoading) {
+    return (
+      <div className={css.container}>
+        <p className={css.loading}>Loading, please wait...</p>
+      </div>
+    );
+  }
+
+  if (error || !note) {
+    return (
+      <div className={css.container}>
+        <p className={css.error}>Something went wrong.</p>
+      </div>
+    );
+  }
+
   return (
     <div className={css.container}>
       <div className={css.item}>
         <div className={css.header}>
-          <h2>{note?.title}</h2>
+          <h2>{note.title}</h2>
         </div>
-        <p className={css.content}>{note?.content}</p>
-        <p className={css.date}>{note?.createdAt}</p>
+        <p className={css.content}>{note.content}</p>
+        <p className={css.date}>{note.createdAt}</p>
       </div>
     </div>
   );
